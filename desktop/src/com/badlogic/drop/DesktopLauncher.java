71c8cd7f8d1e7e896479d15e0c0b76941c265ffb
rohan209547mourya <rohan.209547@gmail.com>
2022-09-25 20:07:27 +0530

    first build

---
+++ desktop/src/com/badlogic/drop/DesktopLauncher.java
@@ -0,0 +1,14 @@
+package com.badlogic.drop;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.badlogic.drop.Drop;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("Bucket and Drop - Rohan Mourya");
+		new Lwjgl3Application(new Drop(), config);
+	}
+}
